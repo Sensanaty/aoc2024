b3a8c59e9a6ba08c13275801c12830cfdf0a316a
Luka <salevic@lu-ka.me>
2024-12-03 15:36:28 +0100

    Day 1 part 1 naive solution

--- 01/01.rb
+++ 01/01.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 require_relative '../file_helper'
-input = FileHelper.read_input
 
+def part_1
+  input = FileHelper.read_input
 
+  left = []
+  right = []
+
+  input.each_line do |line|
+    l, r = line.split(/\s+/).map(&:to_i)
+    left << l
+    right << r
+  end
+
+  left.sort!
+  right.sort!
+
+  distance = 0
+  left.zip(right).sum do |l, r|
+    distance += (l - r).abs
+  end
+
+  distance
+end
+
+puts "Part 1: #{part_1}"
