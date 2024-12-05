1aec19f70f8ca2301a5761ec8e92ab7f424d6b73
Luka <salevic@lu-ka.me>
2024-12-05 11:06:43 +0100

    More idiomatic Ruby solution

--- 04/04.rb
+++ 04/04.rb
@@ -6,57 +6,33 @@ INPUT = FileHelper.read_input
 
 def part_one
   word = 'XMAS'
-  count = 0
   grid = INPUT.split("\n").map(&:chars)
 
-  direction = [
-    # Right
-    [0, 1],
-    # Left
-    [0, -1],
-    # Up
-    [1, 0],
-    # Down
-    [-1, 0],
-    # Up Right
-    [1, 1],
-    # Up Left
-    [1, -1],
-    # Down Right
-    [-1, 1],
-    # Down Left
-    [-1, -1]
-  ]
+  directions = {
+    right: [0, 1],
+    left: [0, -1],
+    up: [1, 0],
+    down: [-1, 0],
+    up_right: [1, 1],
+    up_left: [1, -1],
+    down_right: [-1, 1],
+    down_left: [-1, -1]
+  }
 
-  grid.each_with_index do |row, row_index|
-    row.each_with_index do |_col, col_index|
-      direction.each do |dir|
-        is_xmas = true
+  grid.each_index.sum do |row|
+    grid[row].each_index.sum do |col|
+      directions.values.count do |row_dir, col_dir|
+        word.each_char.with_index.all? do |char, char_index|
+          row_offset = row + (row_dir * char_index)
+          col_offset = col + (col_dir * char_index)
 
-        x = dir[0]
-        y = dir[1]
-
-        word.each_char.with_index do |char, char_index|
-          row_offset = row_index + (x * char_index)
-          col_offset = col_index + (y * char_index)
-
-          if row_offset.negative? || row_offset >= grid.count || col_offset.negative? || col_offset >= grid[row_index].count
-            is_xmas = false
-            break
-          end
-
-          if grid[row_offset][col_offset] != char
-            is_xmas = false
-            break
-          end
+          row_offset.between?(0, grid.length - 1) &&
+            col_offset.between?(0, grid[0].length - 1) &&
+            grid[row_offset][col_offset] == char
         end
-
-        count += 1 if is_xmas
       end
     end
   end
-
-  count
 end
 
 p part_one
