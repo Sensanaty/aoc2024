0a29ce942fa7e798f2cf1fb9d3eb205a284f7bdb
Luka <salevic@lu-ka.me>
2024-12-05 13:28:20 +0100

    Naive day 4 part 2 solution

--- 04/04.rb
+++ 04/04.rb
@@ -35,3 +35,56 @@ def part_one
 end
 
 p part_one
+
+def part_two
+  count = 0
+
+  directions = [
+    # northwest to southeast diagonal
+    [[-1, 1], [1, -1]],
+    # northeast to southwest diagonal
+    [[-1, -1], [1, 1]]
+  ]
+
+  INPUT.each_with_index do |_row, row_index|
+    INPUT[row_index].each_with_index do |_col, col_index|
+      next unless INPUT[row_index][col_index] == 'A'
+
+      is_xmas = true
+
+      directions.each do |direction|
+        row_one_index = row_index + direction[0][0]
+        col_one_index = col_index + direction[0][1]
+
+        row_two_index = row_index + direction[1][0]
+        col_two_index = col_index + direction[1][1]
+
+        if row_one_index.negative? ||
+           row_one_index > INPUT.length - 1 ||
+           col_one_index.negative? ||
+           col_one_index > INPUT[row_index].length - 1 ||
+           row_two_index.negative? ||
+           row_two_index > INPUT.length - 1 ||
+           col_two_index.negative? ||
+           col_two_index > INPUT[row_index].length - 1
+          is_xmas = false
+          break
+        end
+
+        if (INPUT[row_one_index][col_one_index] == 'M' && INPUT[row_two_index][col_two_index] == 'S') ||
+           (INPUT[row_one_index][col_one_index] == 'S' && INPUT[row_two_index][col_two_index] == 'M')
+          next
+        end
+
+        is_xmas = false
+        break
+      end
+
+      count += 1 if is_xmas
+    end
+  end
+
+  count
+end
+
+p part_two
